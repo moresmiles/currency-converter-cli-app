fc0fc20199de0d86274539e0d24fa84cf5ea05c9
github username <github email address>
2017-10-01 12:27:51 +0300

    defined basic menu method and asks user for input

--- lib/currency_converter/cli.rb
+++ lib/currency_converter/cli.rb
@@ -4,4 +4,8 @@ class CurrencyConverter::CLI
   def greeting
     puts "Welcome to Currency Converter!"
   end
+  def menu
+    puts "To see our list of major currency pairs, their conversion rates and more information enter 1. To read our disclaimer about currency conversion enter 2, to exit enter 3"
+    input = gets.strip
+  end
 end

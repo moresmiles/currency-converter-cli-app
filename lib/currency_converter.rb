a772d10234bdac46ab1e55e69e3edae33899c86b
github username <github email address>
2017-10-01 12:15:02 +0300

    required cli file in the environment

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -1,5 +1,8 @@
 require "currency_converter/version"
+#acting as environment
+require_relative "./currency_converter/version"
 
 module CurrencyConverter
   # Your code goes here...
 end
+require_relative './currency_converter/cli'

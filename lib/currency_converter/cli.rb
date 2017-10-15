dc776f10e383e38accb156bf4bf7f22b6acf3f8b
github username <github email address>
2017-10-15 12:21:27 +0300

    made menu method into a loop and added case statement for user input

--- lib/currency_converter/cli.rb
+++ lib/currency_converter/cli.rb
@@ -5,7 +5,23 @@ class CurrencyConverter::CLI
     puts "Welcome to Currency Converter!"
   end
   def menu
+    input = nil
+    while input != "3"
     puts "To see our list of major currency pairs, their conversion rates and more information enter 1. To read our disclaimer about currency conversion enter 2, to exit enter 3"
     input = gets.strip
+    if input.to_i
+      case input
+      when "1"
+        list_pairs
+        choose_pair
+      when "2"
+        disclaimer
+      when "3"
+        quit
+      else
+        puts "I'm sorry that is not a valid entry."
+      end
+      end
+    end
   end
 end

dbd4bf1ddf0d5593ad6791531db6884506427043
github username <github email address>
2017-10-16 09:53:55 +0300

    defined #self.currency_scraper which returns array of currency pairs

--- lib/currency_converter/pairs.rb
+++ lib/currency_converter/pairs.rb
@@ -1,3 +1,19 @@
 class CurrencyConverter::Pairs
   attr_accessor :name, :value, :net_change, :time
+
+  def self.currency_scraper
+
+    doc = Nokogiri::HTML(open("https://www.bloomberg.com/markets/currencies"))
+    pairs_array = []
+    doc.search("tr.data-table-row").each do |row|
+
+      pair = self.new
+      pair.name = row.search("div.data-table-row-cell__link-block").text.strip
+      pair.value = row.search("td.data-table-row-cell[data-type='value']").text.strip
+      pair.net_change = row.search("td.data-table-row-cell.hide-on-mobile").first.text.strip
+      pair.time = row.search("td.data-table-row-cell.hide-on-mobile[data-type='time']").text.strip
+      pairs_array << pair
+    end
+    pairs_array
+  end
 end
